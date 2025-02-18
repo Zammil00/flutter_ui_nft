4bb80ced3629e3c7b960b301b54c9808b7a42d0d
INI_LABIL <115417258+Zammil00@users.noreply.github.com>
2025-02-18 17:16:59 +0700

    Create Widget Page

--- lib/pages/get_started_page.dart
+++ lib/pages/get_started_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import '../widget/page_widget.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return PageWidget(
+      child: Text("HAALO"),
+    );
   }
 }
